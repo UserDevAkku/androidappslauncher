9d8d864609ba46eb62fe9ec61a5ec4e5d35f3a90
UserDevAkku <aakashverma.1234.abcd@gmail.com>
2024-11-21 20:58:43 +0530

    updated  files

--- aal.sh
+++ aal.sh
@@ -29,7 +29,6 @@ showList() {
     for ((i = 0; i < lengthofarrLabel; i++)); do
         echo -e "\033[1;93m\$((i + 1))\033[0m\033[1;38;5;156m.\033[0m\033[1;38;5;39m\${arrLabel[\$i]}\033[0m"
     done
-    printf "\n"
     gum style \$'\033[1;38;5;124m✦ \033[0m\033[1;93mUSE SERIAL NUMBER TO LAUNCH THE APP\033[0m\033[1;38;5;124m ✦\033[0m' --width "\$width" --height 1 --bold --align center
 }
 showList
